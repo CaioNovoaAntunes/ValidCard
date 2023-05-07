f8a311e9732984abb94769e0d5ef43587137753f
CaioNovoaA <caio.antunes@zup.com.br>
2023-05-06 23:53:31 -0300

    Pegando binding result de forma genérica utilizando as ferramentas do spring

--- src/main/java/com/br/cardvalidator/CartaoController.java
+++ src/main/java/com/br/cardvalidator/CartaoController.java
@@ -1,19 +1,24 @@
 package com.br.cardvalidator;
 
+import com.br.cardvalidator.dto.CardRequest;
 import com.br.cardvalidator.exception.CartaoInvalidoException;
 import com.br.cardvalidator.model.Card;
 import com.br.cardvalidator.repository.CartaoRepository;
 import com.br.cardvalidator.service.CartaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping
@@ -27,18 +32,18 @@ public class CartaoController {
     }
 
     @PostMapping("/cartoes")
-    public ResponseEntity salvarCartao(@Valid @RequestBody Card cartao) {
-        try {
-            if (cartao.getNomeTitular() == null || cartao.getNomeTitular().isEmpty()) {
-                throw new CartaoInvalidoException("O número do cartão não pode estar vazio");
-            }
-            // Restante da lógica para salvar o cartão na base de dados
-
-            return ResponseEntity.status(HttpStatus.CREATED).body("Cartão salvo com sucesso");
-
-        } catch (CartaoInvalidoException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<?> salvarCartao(@Valid @RequestBody CardRequest cartao, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
         }
+        Card card = cartao.toModel();
+
+            cartaoRepository.save(card);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Cartão salvo com sucesso");
     }
 
 }
